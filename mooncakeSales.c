5279f90acd26e127b545fcb5c62da8e2afc74f66
HT4w5 <example@example.com>
2024-11-15 15:27:36 +0800

    new file:   mooncakeSales.c

---
+++ mooncakeSales.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+  mcake_t:
+  inv: inventory.
+  tp:  total price.
+  up:  unit price.
+*/
+typedef struct
+{
+    double inv;
+    double tp;
+    double up;
+} mcake_t;
+
+double max_income(mcake_t mcakes[], int n_mcake, int demand)
+{
+
+}
